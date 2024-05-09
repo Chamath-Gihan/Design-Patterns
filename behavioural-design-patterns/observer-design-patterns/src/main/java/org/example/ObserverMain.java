edbf0b1868278abaf2c56fc6ab02fc47e40d5df3
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2024-05-09 12:22:41 +0530

    Observer Design Pattern

---
+++ behavioural-design-patterns/observer-design-patterns/src/main/java/org/example/ObserverMain.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class ObserverMain {
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+        new BinaryObserver(subject);
+        new HexaObserver(subject);
+
+        System.out.println("State : 10");
+        subject.setState(10);
+        System.out.println("State : 5");
+        subject.setState(5);
+    }
+}
