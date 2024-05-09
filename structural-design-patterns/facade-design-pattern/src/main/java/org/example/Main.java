b5c53ee65e2d9622d7954ea99a6dac7c783d8300
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2024-05-09 10:49:31 +0530

    Facade Design Pattern

---
+++ structural-design-patterns/facade-design-pattern/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+    }
+}
---
+++ structural-design-patterns/facade-design-pattern/src/main/java/org/example/ShapeMaker.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class ShapeMaker {
+
+    private Shape circle;
+
+    private Shape rectangle;
+
+    public ShapeMaker(){
+        circle = new Circle();
+        rectangle = new Rectangle();
+    }
+
+    public void drawCircle(){
+        circle.draw();
+    }
+
+    public void drawRectangle(){
+        rectangle.draw();
+    }
+}
