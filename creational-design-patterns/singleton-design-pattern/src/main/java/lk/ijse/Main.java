3c8196c92c4df5e05268c4c30b61c8b64e7996ab
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2024-05-09 10:02:12 +0530

    Change the peoject some  modules

--- creational-design-patterns/singleton-design-pattern/src/main/java/lk/ijse/Main.java
+++ creational-design-patterns/singleton-design-pattern/src/main/java/lk/ijse/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(Singleton.getSingleton());
         System.out.println(Singleton.getSingleton());
-        System.out.println(Singleton.getSingleton());
     }
 }
